086b405ec5ce1a89650d4be2bba25c10710c0848
Arrox-Gusev <arrox2228@gmail.com>
2025-01-28 11:56:36 +0300

    Удаление не нужных файлов с джанго

--- predprof_project/notes_to_music/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Item
-
-
-def test(request):
-    items = Item.objects.all()
-    return render(request, 'index.html')
--- predprof_project/predprof_project/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-    path('accounts/', include('accounts.urls')),
-    path('', include('notes_to_music.urls')),
-]
